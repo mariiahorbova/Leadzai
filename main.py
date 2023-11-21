924cbd23b6f143de3a5a9e2dfb0826f66655e288
Mariia Horbova <gorbova21@gmail.com>
2023-11-21 15:07:47 +0200

    Updated main functionality, updated tests to cover more edge cases

--- main.py
+++ main.py
@@ -1,29 +1,122 @@
-def generate_pagination(current_page, total_pages, boundaries, around):
-    if total_pages <= 0:
-        raise ValueError("Total pages must be non-negative number")
-    if current_page <= 0:
-        raise ValueError("Current page must be non-negative number")
-    if current_page > total_pages:
-        raise ValueError("Current page must be in range of total pages")
-    if boundaries < 0:
-        raise ValueError("Boundaries must be non-negative number")
-    if around < 0:
-        raise ValueError("Around must be non-negative number")
-    hidden_pages = "..."
-    pages = [i for i in range(1, total_pages + 1)]
-    pages_start = pages[: boundaries]
-    pages_end = pages[-boundaries:]
-    around_current_page = pages[
-        pages.index(current_page) - around: pages.index(current_page) + around + 1
-    ]
-    result = list(set(pages_start + around_current_page + pages_end))
-    previous_page = result[0]
-    result_string = str(previous_page)
-    for page in result[1:]:
-        if page - previous_page == 1:
-            result_string += " " + str(page)
-        else:
-            result_string += " " + hidden_pages + " " + str(page)
-        previous_page = page
-    print(result_string)
-    return result_string
+class PaginationGenerator():
+
+    ELLIPSIS = "..."   
+
+    def validate_input(self, current_page, total_pages, boundaries, around):
+        all_arguments = [current_page, total_pages, boundaries, around]
+
+        for argument in all_arguments:
+            if not type(argument) is int:
+                raise TypeError(
+                    f"All variables must be integers. "
+                    f" {argument} is not an integer.")
+        if total_pages <= 0:
+            raise ValueError("Total pages must be non-negative number")
+        if current_page <= 0:
+            raise ValueError("Current page must be non-negative number")
+        if current_page > total_pages:
+            raise ValueError("Current page must be in range of total pages")
+        if boundaries < 0:
+            raise ValueError("Boundaries must be non-negative number")
+        if around < 0:
+            raise ValueError("Around must be non-negative number")
+
+    def simple_generation(self, current_page, total_pages, boundaries, around):
+        if around >= total_pages or boundaries * 2 >= total_pages or around * 2 >= total_pages:
+            result_list = list(range(1, total_pages + 1))
+            return result_list
+        return None
+
+    def get_base_values(self, current_page, total_pages, boundaries, around):
+        boundaries_start_last_page = boundaries
+        boundaries_end_first_page = total_pages - boundaries + 1
+        
+        around_current_page_start = current_page - around if current_page - around > 0 else 1
+        around_current_page_end = current_page + around if current_page + around < total_pages else total_pages
+        
+        around_current_page_list = range(around_current_page_start, around_current_page_end + 1)
+        return (
+            boundaries_start_last_page, 
+            boundaries_end_first_page, 
+            around_current_page_list,
+        )
+
+    def generate_pagination(self, current_page, total_pages, boundaries, around):
+        result_list = []
+
+        self.validate_input(current_page, total_pages, boundaries, around)
+        simple_pagination_list = self.simple_generation(current_page, total_pages, boundaries, around)
+
+        if simple_pagination_list:
+            return " ".join(map(str, simple_pagination_list))
+
+        (
+            boundaries_start_last_page,
+            boundaries_end_first_page,
+            around_current_page_list
+        ) = self.get_base_values(current_page, total_pages, boundaries, around)
+
+        start = self.check_start(
+            current_page,
+            around,
+            boundaries_start_last_page,
+            around_current_page_list,
+            boundaries
+        )
+
+        end = self.check_end(
+            current_page,
+            around,
+            boundaries_end_first_page,
+            around_current_page_list,
+            total_pages
+        )
+
+        result_list.extend(start)
+        result_list.extend(end)
+
+        return " ".join(map(str, result_list))
+
+    def check_start(
+        self,
+        current_page, 
+        around, 
+        boundaries_start_last_page, 
+        around_current_page_list,
+        boundaries,
+    ):
+        result_list = []
+
+        if boundaries <= 0 and around_current_page_list[0] == 1:
+            result_list.extend(around_current_page_list)
+
+        elif current_page - around > boundaries_start_last_page:
+            result_list.extend(range(1, boundaries_start_last_page + 1))
+            result_list.append(self.ELLIPSIS)
+            result_list.extend(around_current_page_list)
+
+        elif current_page - around <= boundaries_start_last_page:
+            result_list.extend(range(1, around_current_page_list[0]))
+            result_list.extend(around_current_page_list)
+        
+        return result_list
+
+    def check_end(
+        self,
+        current_page,
+        around,
+        boundaries_end_first_page, 
+        around_current_page_list, 
+        total_pages,
+    ):
+        result_list = []
+
+        if boundaries_end_first_page - around_current_page_list[-1] <= 1:
+            result_list.extend(range(around_current_page_list[-1] + 1, total_pages + 1))
+            return " ".join(map(str, result_list))
+
+        elif current_page + around < boundaries_end_first_page:
+            result_list.append(self.ELLIPSIS)
+            result_list.extend(range(boundaries_end_first_page, total_pages + 1))
+
+        return result_list
--- tests.py
+++ tests.py
@@ -1,12 +1,55 @@
 import unittest
-from main import generate_pagination
-
+from main import PaginationGenerator
 
 class TestPagination(unittest.TestCase):
     def setUp(self):
-        self.pagination = generate_pagination
+        self.generator = PaginationGenerator()
+        self.pagination = self.generator.generate_pagination
 
     def test_pagination_is_correct(self):
+
+        expected = "1 2 3 4 ... 30 31 32 33 34 35 36 ... 62 63 64 65"
+        result = self.pagination(33, 65, 4, 3)
+        self.assertEqual(result, expected)
+    
+    def test_pagination_around_gte_total_pages(self):
+        
+        expected = "1 2 3 4 5 6 7 8 9 10"
+        result = self.pagination(5, 10, 1, 10)
+        self.assertEqual(result, expected)
+
+        expected = "1 2 3 4 5 6 7 8 9 10"
+        result = self.pagination(5, 10, 1, 11)
+        self.assertEqual(result, expected)
+
+    def test_pagination_all_values_the_same(self):
+
+        expected = "1 2 3 4 5 6 7 8 9 10"
+        result = self.pagination(10, 10, 10, 10)
+        self.assertEqual(result, expected)
+    
+    def test_pagination_boundary_eq_around(self):
+        
+        expected = "1 2 3 4 5 6 ... 9 10"
+        result = self.pagination(4, 10, 2, 2)
+        self.assertEqual(result, expected)
+    
+    def test_pagination_zero_boundaries(self):
+
+        expected = "... 4 5 6 ..."
+        result = self.pagination(5, 10, 0, 1)
+        self.assertEqual(result, expected)
+        
+        expected = "... 8 9 10"
+        result = self.pagination(9, 10, 0, 1)
+        self.assertEqual(result, expected)
+
+        expected = "1 2 3 4 5 ..."
+        result = self.pagination(3, 10, 0, 2)
+        self.assertEqual(result, expected)
+
+    def test_pagination_zero_around(self):
+        
         expected = "1 ... 4 5"
         result = self.pagination(4, 5, 1, 0)
         self.assertEqual(result, expected)
@@ -35,7 +78,22 @@ class TestPagination(unittest.TestCase):
     def test_pagination_around_negative(self):
         with self.assertRaises(ValueError):
             self.pagination(4, 10, 1, -1)
+    
+    def test_pagination_current_page_not_int(self):
+        with self.assertRaises(TypeError):
+            self.pagination("0", 10, 1, 1)
 
+    def test_pagination_total_pages_not_int(self):
+        with self.assertRaises(TypeError):
+            self.pagination(4, (0, ), 1, 1)
+
+    def test_pagination_boundaries_not_int(self):
+        with self.assertRaises(TypeError):
+            self.pagination(4, 10, {1: " "}, 1)
+
+    def test_pagination_around_not_int(self):
+        with self.assertRaises(TypeError):
+            self.pagination(4, 10, 1, [2, 3])
 
 if __name__ == "__main__":
     unittest.main()
